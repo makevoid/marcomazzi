630a18b601da75c3a81a771cef04fb294ea5ced2
Francesco Canessa <makevoid@gmail.com>
2012-11-16 16:30:06 +0900

    nuove foto

--- public/img/home/resize.rb
+++ public/img/home/resize.rb
@@ -1,7 +1,13 @@
 path = File.expand_path "../", __FILE__
-photos = Dir.glob("#{path}/*.jpg")
+photos = Dir.glob("#{path}/*.TIF")
 puts photos
 
 photos.each_with_index do |photo, idx|
-  `sips --resampleWidth 1600 --resampleHeight 1600 #{photo}`
+  # `sips --resampleWidth 1600 --resampleHeight 1600 #{photo}`
+  cmd = "convert #{photo}  -geometry 2000x -density 72 -colorspace gray #{idx}.jpg"
+  puts cmd
+  `#{cmd}`
+
+  `rm -f #{idx}-1.jpg`
+  `mv #{idx}-0.jpg #{idx}.jpg`
 end
